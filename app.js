7b2ae6cab795b46bdf1fecdd7344a699a857b479
andito28 <andito763@gmail.com>
2021-03-13 21:25:14 +0800

    fungsi menambahkan todo-list dari inputan

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const todoForm = document.querySelector("#form-todo");
-const todoInput = document.querySelector("input-todo");
+const todoInput = document.querySelector("#input-todo");
+const todoList = document.querySelector("#todo-list")
 
 todoForm.addEventListener("submit", addTodo);
 
 function addTodo(e){
+
     e.preventDefault();
 
-    console.log("mantap");
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+
+    li.className = "list-group-item mb-1 d-flex align-items-center justify-content-between";
+    li.appendChild(document.createTextNode(todoInput.value));
+
+    a.href = "#";
+    a.className = "badge badge-danger";
+
+    a.innerHTML = "Delete";
+
+    li.appendChild(a);
+
+    todoList.appendChild(li);
+    
 }
